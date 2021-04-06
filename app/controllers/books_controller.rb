a3a8220c42810da7c1e03cd8f2e5b8679c28fedc
Christian Lampl <christian.lampl@gmx.at>
2021-04-06 16:32:26 -0400

    Add: navbar with session-related links; make login mandatory

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: %i[ show edit update destroy reserve unreserve pay unpay ]
+  before_action :authenticate_user!
 
   # GET /books or /books.json
   def index
